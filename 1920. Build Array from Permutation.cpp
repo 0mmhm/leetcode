86b9b48c68ae31d2691e533e53886bb077a2b779
Monjurul Huda Munna <monjurulhuda@gmail.com>
2021-09-08 00:09:16 +0600

    Solved 1920 using O(1) space

--- 1920. Build Array from Permutation.cpp
+++ 1920. Build Array from Permutation.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
         int i, len = nums.size();
-        vector<int> res;
         for( i=0 ; i<len ; i++ ) {
-            res.push_back(nums[nums[i]]);
+            nums[i] += len * (nums[nums[i]] % len);
         }
         
-        return res;
+        for( i=0 ; i<len ; i++ ) {
+            nums[i] /= len;
+        }
+        
+        return nums;
     }
 };
