0648582653ff04c6a5541c1d8f409eb76e612fbd
Monjurul Huda Munna <monjurulhuda@gmail.com>
2021-12-25 00:31:14 +0600

    Optimized 814

--- 814. Binary Tree Pruning.cpp
+++ 814. Binary Tree Pruning.cpp
@@ -11,33 +11,23 @@
  */
 class Solution {
 public:
-    int pruneSubTree(TreeNode* node) {
-        if ( !node ) return 0;
+    bool pruneSubTree(TreeNode* node) {
+        if ( !node ) return false;
         
-        int left, right;
-        left = right = 0;
+        bool left, right;
+        left = right = false;
+        
         if( node->left ) left = pruneSubTree(node->left);
         if( node->right ) right = pruneSubTree(node->right);
         
-        if(  node->left && !left ){
-            delete(node->left);
-            node->left = NULL;
-        }
-        if( node->right && !right ){
-            delete(node->right);
-            node->right = NULL;
-        }
+        if( !left ) node->left = NULL;
+        if( !right ) node->right = NULL;
         
-        return node->val + left + right;
+        
+        return node->val || left || right;
     }
     
     TreeNode* pruneTree(TreeNode* root) {
-        int val = pruneSubTree(root);
-        if(!val ) {
-            delete(root);
-            return NULL;
-        }
-        
-        return root;
+        return pruneSubTree(root) ? root : NULL;
     }
 };
