f6c16e32b3fe3032367b5b09aa892ed892d4ba15
Monjurul Huda Munna <monjurulhuda@gmail.com>
2021-12-29 00:16:48 +0600

    Optimized 64

--- 64. Minimum Path Sum.cpp
+++ 64. Minimum Path Sum.cpp
@@ -1,5 +1,27 @@
 class Solution {
 public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int i, j, m, n;
+        
+        m = grid.size();
+        n = grid[0].size();
+        
+        for( i=1 ; i<n ; i++ ) grid[0][i] += grid[0][i-1];
+        for( i=1 ; i<m ; i++ ) grid[i][0] += grid[i-1][0];
+        
+        for( i=1 ; i<m ; i++ ) {
+            for( j=1 ; j<n ; j++ ){
+                grid[i][j] += min( grid[i-1][j], grid[i][j-1] );
+            }
+        }
+        
+        return grid[m-1][n-1];
+    }
+};
+
+/*
+class Solution {
+public:
     int dir[2][2] = {{0, 1}, {1, 0}}; 
     int m, n, minSum = 40000000;
     int mark[201][201];
@@ -46,3 +68,4 @@ public:
         return minSum;
     }
 };
+*/
